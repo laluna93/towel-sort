5661345de9d1a3f9256e551e57ca2ccc9287cf4f
Anna <Laluna-93@yandex.ru>
2022-02-14 12:33:58 +0400

    init: start task towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,32 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let arr = matrix;
+  let arr2;
+let arr3=[]
+let arr4 =[];
+let arr5;
+let number;
+if(arguments.length==0){
+  return []
+}if(Array.isArray(matrix)){
+  if(matrix.length ===0){
+    return []
+  }
+}
+if(arr !== [] && arr !== undefined && arr !=NaN){
+for(let i =0; i<arr.length; i++){
+  arr3=[]
+    for(let k =0; k <arr[i].length;k++){ 
+        arr2 = i%2===0? k : (arr[i].length-k-1) 
+         arr3.push(arr[i][arr2])
+    } 
+    arr4.push(arr3)
+} 
+arr5 = arr4.join()
+let b=arr5.split(',')
+ number = b.map(parseFloat);
+console.log(number) 
+return number = b.map(parseFloat)
+}
 }
